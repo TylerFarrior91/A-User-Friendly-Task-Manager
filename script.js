38f80324aed45dbea3a483c12beca50276b568db
Tyler Farrior <tylerfarrior@gmail.com>
2023-12-23 14:06:48 -0500

    adding function to JS file

---
+++ script.js
@@ -0,0 +1,6 @@
+function addTask() {
+    var taskInput = document.getElementById("new-task");
+    var dueDateInput = document.getElementById("due-date");
+    
+    var taskText = taskInput.value;
+    var dueDate = dueDateInput.value;
